b704cc8f72ca3d5ff774e5d7250e609a380592e4
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2023-03-29 19:16:28 +0100

    Implemented use of mouse for movement

--- WinterbourneBabysitter/Assets/Scripts/PlayerMovement.cs
+++ WinterbourneBabysitter/Assets/Scripts/PlayerMovement.cs
@@ -1,35 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Player Speed
     public float speed;
     private Rigidbody2D playerRB;
+    private NavMeshAgent playerNMA;
     private Vector2 velocity;
+    private Vector2 movementInput;
+    private Vector3 target;
+    private bool usingMouse;
 
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        playerNMA = GetComponent<NavMeshAgent>();
+        target = transform.position;
+        usingMouse = true;
     }
 
     void Update()
     {
-        // Get player's Key-press movement input
-        Vector2 movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        // Calculate Velocity
-        velocity = movementInput.normalized * speed;
+        if (Input.GetMouseButtonDown(0))
+        {
+            usingMouse = true;
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+        else if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0 || Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
+        {
+            // Get player's Key-press movement input
+            usingMouse = false;
+            movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            velocity = movementInput.normalized * speed;
+            playerRB.MovePosition(playerRB.position + velocity * Time.fixedDeltaTime);
+        }
+
+        if (usingMouse) { transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime); }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         velocity = velocity * 0;
-    }
-
-
-    void FixedUpdate()
-    {
-        playerRB.MovePosition(playerRB.position + velocity * Time.fixedDeltaTime);
+        target = transform.position;
     }
 }
